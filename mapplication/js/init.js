06e77eb75b23dac7977941077e896f1bb8243b5f
stinest <justineclim@gmail.com>
2024-07-28 17:55:33 -1000

    updated layout and map

--- mapplication/js/init.js
+++ mapplication/js/init.js
@@ -8,57 +8,80 @@ const map = new maplibregl.Map({
     zoom: mapOptions.startingZoomLevel
 });
 
+let markers = {
+    encampment: [],
+    protest: [],
+    significant: []
+};
+
+const uniqueEncampmentLocations = new Set();
+const uniqueProtestLocations = new Set();
+const uniqueSignificantLocations = new Set();
+
 function addMarker1(data){
-    let popup_message;
     let lng = data['lng1'];
     let lat = data['lat1'];
-    popup_message = `<h2>Protest Site</h2> <h3>Location: ${data['Could you best specify the location where you were present?']}</h3>`
+    let locationKey = `${lng},${lat}`;
 
-    new maplibregl.Marker({ element: createImage('signIcon') })
-        .setLngLat([lng, lat])
-        .setPopup(new maplibregl.Popup()
-            .setHTML(popup_message))
-        .addTo(map)
-    createButtons(1, lat,lng,data['Could you best specify the location where you were present?']);
+    if (!uniqueProtestLocations.has(locationKey)) {
+        uniqueProtestLocations.add(locationKey);
+
+        let popup_message = `<h2>Protest Site</h2> <h3>Location: ${data['Could you best specify the location where you were present?']}</h3>`
+
+        let marker = new maplibregl.Marker({ element: createImage('signIcon') })
+            .setLngLat([lng, lat])
+            .setPopup(new maplibregl.Popup()
+                .setHTML(popup_message))
+            .addTo(map)
+        markers.protest.push(marker);
+        createButtons(1, lat, lng, data['Could you best specify the location where you were present?']);
+    }
 }
 
 function addMarker2(data){
-    let popup_message;
     let lng = data['lng2'];
     let lat = data['lat2'];
-    popup_message = `<h2>Encampment Site</h2> <h3>Location: ${data['If involved at any encampments related to the Pro-Palestine movement, could you best specify the location where you were present?']}</h3>`
+    let locationKey = `${lng},${lat}`;
+
+    if (!uniqueEncampmentLocations.has(locationKey)) {
+        uniqueEncampmentLocations.add(locationKey);
+
+    let popup_message = `<h2>Encampment Site</h2> <h3>Location: ${data['If involved at any encampments related to the Pro-Palestine movement, could you best specify the location where you were present?']}</h3>`
  
-    new maplibregl.Marker({ element: createImage('tentIcon') })
+    let marker = new maplibregl.Marker({ element: createImage('tentIcon') })
         .setLngLat([lng, lat])
         .setPopup(new maplibregl.Popup()
             .setHTML(popup_message))
         .addTo(map)
-    createButtons(2, lat,lng,data['If involved at any encampments related to the Pro-Palestine movement, could you best specify the location where you were present?']);
+    console.log(popup_message)
+    markers.encampment.push(marker);
+    createButtons(2, lat, lng, data['If involved at any encampments related to the Pro-Palestine movement, could you best specify the location where you were present?']);
+    }
 }
 
 function addMarker3(data){
-    let popup_message;
     let lng = data['lng3'];
     let lat = data['lat3'];
-    popup_message = `<h2>Significant Landmark</h2> <h3>Location: ${data["Can you describe any specific location on UCLA's campus that have become significant in the context of Pro-Palestine movements?"]}</h3>`
+    let locationKey = `${lng},${lat}`;
 
-    new maplibregl.Marker({ element: createImage('flagIcon') })
+    if (!uniqueSignificantLocations.has(locationKey)) {
+        uniqueSignificantLocations.add(locationKey);
+
+    let popup_message = `<h2>Significant Landmark</h2> <h3>Location: ${data["Can you describe any specific location on UCLA's campus that have become significant in the context of Pro-Palestine movements?"]}</h3>`
+
+    let marker = new maplibregl.Marker({ element: createImage('flagIcon') })
         .setLngLat([lng, lat])
         .setPopup(new maplibregl.Popup()
             .setHTML(popup_message))
         .addTo(map)
-    createButtons(3, lat,lng,data["Can you describe any specific location on UCLA's campus that have become significant in the context of Pro-Palestine movements?"]);
+    console.log(popup_message)
+    markers.significant.push(marker);
+    createButtons(3, lat, lng, data["Can you describe any specific location on UCLA's campus that have become significant in the context of Pro-Palestine movements?"]);
+    }
 }
 
 function createButtons(markerNum, lat,lng,title){
-    const buttonId = "button" + title.replace(/\s+/g, '_'); 
-    const existingButton = document.getElementById(buttonId);
-    if (existingButton) {
-        return; // for no duplicate buttons
-    }
-
     const newButton = document.createElement("button");
-    newButton.id = buttonId;
     newButton.innerHTML = title;
     newButton.setAttribute("lat", lat);
     newButton.setAttribute("lng", lng);
@@ -69,10 +92,12 @@ function createButtons(markerNum, lat,lng,title){
     });
 
     // appends to corresponding buttonmap
-    document.getElementById("buttonmap").appendChild(newButton);
-    //document.getElementById("encampmentbuttons").appendChild(newButton);
-    //document.getElementById("protestbuttons").appendChild(newButton);
-    //document.getElementById("significantbuttons").appendChild(newButton);
+    if (markerNum == 1)
+        document.getElementById("protestbuttons").appendChild(newButton);
+    else if (markerNum == 2)
+        document.getElementById("encampmentbuttons").appendChild(newButton);
+    else if (markerNum == 3)
+        document.getElementById("significantbuttons").appendChild(newButton);
 }
 
 function createImage(img) {
@@ -97,15 +122,12 @@ function createImage(img) {
 
 const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPNJwVJXo0yXNrpejSqaLoJmkjabEdnKbI47-VEL-WX7FUofONme430LdJLNV9ZTTPftO1_HNXJJdt/pub?output=csv"
 
-// When the map is fully loaded, start adding GeoJSON data
 map.on('load', function() {
-    // Use PapaParse to fetch and parse the CSV data from a Google Forms spreadsheet URL
     Papa.parse(dataUrl, {
-        download: true, // Tells PapaParse to fetch the CSV data from the URL
-        header: true, // Assumes the first row of your CSV are column headers
+        download: true,
+        header: true,
         complete: function(results) {
-            // Process the parsed data
-            processData(results.data); // Use a new function to handle CSV data
+            processData(results.data); 
         }
     });
 });
@@ -117,12 +139,25 @@ function processData(results){
         // assumes your geojson has a "title" and "message" attribute
         // let coordinates = feature.geometry.coordinates;
         if (feature['Are you comfortable with sharing your story?'] == "Yes") {
-            if (feature['lat1'] != "0")
-                addMarker1(feature);
-            if (feature['lat2'] != "0")
-                addMarker2(feature);
-            if (feature['lat3'] != "0")
-                addMarker3(feature);
+            if (feature['lat1'] != "0") addMarker1(feature);
+            if (feature['lat2'] != "0") addMarker2(feature);
+            if (feature['lat3'] != "0") addMarker3(feature);
         }
     });
+    toggleMarkersVisibility();
 };
+
+function toggleMarkersVisibility() {
+    // sees if checked
+    let showEncampment = document.getElementById('showEncampment').checked;
+    let showProtest = document.getElementById('showProtest').checked;
+    let showSignificant = document.getElementById('showSignificant').checked;
+
+    markers.encampment.forEach(marker => marker.getElement().style.display = showEncampment ? 'block' : 'none');
+    markers.protest.forEach(marker => marker.getElement().style.display = showProtest ? 'block' : 'none');
+    markers.significant.forEach(marker => marker.getElement().style.display = showSignificant ? 'block' : 'none');
+}
+
+document.getElementById('showEncampment').addEventListener('change', toggleMarkersVisibility);
+document.getElementById('showProtest').addEventListener('change', toggleMarkersVisibility);
+document.getElementById('showSignificant').addEventListener('change', toggleMarkersVisibility);
