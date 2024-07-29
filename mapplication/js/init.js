3e0ccd33d0cf382cc86639942ee777341f822b7e
stinest <justineclim@gmail.com>
2024-07-29 13:40:29 -1000

    added responses

--- mapplication/js/init.js
+++ mapplication/js/init.js
@@ -165,39 +165,97 @@ document.getElementById('showEncampment').addEventListener('change', toggleMarke
 document.getElementById('showProtest').addEventListener('change', toggleMarkersVisibility);
 document.getElementById('showSignificant').addEventListener('change', toggleMarkersVisibility);
 
-/* responses to-do
-
 function makeResponseCards(data) {
     let timestamp = data['Timestamp'];
     let date = new Date(timestamp);
     let monthYear = `${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`;
 
-    let card = document.createElement('div');
-    card.className = 'card';
+    let responseCard = document.createElement('div');
+    responseCard.className = 'response-card';
 
+    let title = `Response from ${data['How are you/have been affiliated with UCLA?']} (${monthYear}, ${data['Where are you currently located?']})`;
+    let titleElement = document.createElement('h3');
+    titleElement.textContent = title;
+    responseCard.appendChild(titleElement);
 
-    if data['Timestamp'] contains 08/2024
-        let title = Response from data['How are you/have been affiliated with UCLA?'] (August 2024, data[Where are you currently located?])
-    if data[How have you supported the Palestinian resistance?] != ""
-        let q1 = How have you supported the Palestinian resistance?
-        let a1 = data[How have you supported the Palestinian resistance?]
-    if data[If involved at any encampments related to the Pro-Palestine movement, could you share your experience?] != ""
-        let q2 = If involved at any encampments related to the Pro-Palestine movement, could you share your experience?
-        let a2= data[If involved at any encampments related to the Pro-Palestine movement, could you share your experience?]
-    if data[Is there anything else you would like to share in regards to your experience advocating for Palestine?] != ""
-        let q3 = Is there anything else you would like to share in regards to your experience advocating for Palestine?
-        let a3 = data[Is there anything else you would like to share in regards to your experience advocating for Palestine?]
-    if data[Has showing solidarity with Palestine impacted you in any way?] != ""
-        let q4 = Has showing solidarity with Palestine impacted you in any way?
-        let a4 = data[Has showing solidarity with Palestine impacted you in any way?]
-    if data[How have recent events affected your perception of UCLA's campus spaces?] != ""
-        let q5 = How have recent events affected your perception of UCLA's campus spaces?
-        let a5 = data[How have recent events affected your perception of UCLA's campus spaces?]
-    if data[How have UCLA's administrative responses to Pro-Palestine activism affected your use of campus spaces?]
-        let q6 = How have UCLA's administrative responses to Pro-Palestine activism affected your use of campus spaces?
-        let a6 = data[How have UCLA's administrative responses to Pro-Palestine activism affected your use of campus spaces?]
-    if data[Is there anything else you would like to share?]
-        let q7 = Is there anything else you would like to share?
-        let a7 = data[Is there anything else you would like to share?]
+    let hasContent = false; // ignores blank forms
+
+    if (data['How have you supported the Palestinian resistance?']) {
+        let q1 = 'How have you supported the Palestinian resistance?';
+        let a1 = data['How have you supported the Palestinian resistance?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q1;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a1;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+
+    if (data['If involved at any encampments related to the Pro-Palestine movement, could you share your experience?']) {
+        let q2 = 'If involved at any encampments related to the Pro-Palestine movement, could you share your experience?';
+        let a2 = data['If involved at any encampments related to the Pro-Palestine movement, could you share your experience?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q2;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a2;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+
+    if (data['Is there anything else you would like to share in regards to your experience advocating for Palestine?']) {
+        let q3 = 'Is there anything else you would like to share in regards to your experience advocating for Palestine?';
+        let a3 = data['Is there anything else you would like to share in regards to your experience advocating for Palestine?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q3;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a3;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+
+    if (data['Has showing solidarity with Palestine impacted you in any way?']) {
+        let q4 = 'Has showing solidarity with Palestine impacted you in any way?';
+        let a4 = data['Has showing solidarity with Palestine impacted you in any way?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q4;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a4;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+
+    if (data['How have recent events affected your perception of UCLA\'s campus spaces?']) {
+        let q5 = 'How have recent events affected your perception of UCLA\'s campus spaces?';
+        let a5 = data['How have recent events affected your perception of UCLA\'s campus spaces?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q5;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a5;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+
+    if (data['How have UCLA\'s administrative responses to Pro-Palestine activism affected your use of campus spaces?']) {
+        let q6 = 'How have UCLA\'s administrative responses to Pro-Palestine activism affected your use of campus spaces?';
+        let a6 = data['How have UCLA\'s administrative responses to Pro-Palestine activism affected your use of campus spaces?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q6;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a6;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+
+    if (data['Is there anything else you would like to share?']) {
+        let q7 = 'Is there anything else you would like to share?';
+        let a7 = data['Is there anything else you would like to share?'];
+        let questionElement = document.createElement('h4');
+        questionElement.textContent = q7;
+        let answerElement = document.createElement('p');
+        answerElement.textContent = a7;
+        responseCard.appendChild(questionElement);
+        responseCard.appendChild(answerElement);
+        hasContent = true; }
+    if (hasContent) {
+        document.getElementById('responses').appendChild(responseCard); }
 }
-*/
